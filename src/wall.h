e91465b888d40def26b6597c3c09e9af9b95efb4
Emil Nilsson <emini901@student.liu.se>
2014-11-27 14:00:21 +0100

    commiting wall

---
+++ src/wall.h
@@ -0,0 +1,32 @@
+/*
+ *  Wall class declaration
+ */
+
+class shot;
+
+#ifndef WALL_H
+#define WALL_H_
+
+
+class Wall {
+private:
+    // Wall position
+    int x;
+    int y;
+
+public:
+    Wall(int x, int y);
+
+public:
+    // Wall dimensions
+    static const int HEIGHT;
+    static const int WIDTH;
+
+    // Functions
+    int get_x();
+    int get_y();
+    //int predict(Shot *shot);
+    //void SHOTCALCULATORFUNCTION(Shot *shot);
+};
+
+#endif
